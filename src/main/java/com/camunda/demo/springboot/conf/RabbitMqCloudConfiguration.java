7ae1b2620329bbd56c2e33d268639d01c5372761
Bernd Rücker <bernd.ruecker@camunda.com>
2017-08-03 15:27:11 +0200

    improved rabbit config for cloud

--- src/main/java/com/camunda/demo/springboot/conf/RabbitMqCloudConfiguration.java
+++ src/main/java/com/camunda/demo/springboot/conf/RabbitMqCloudConfiguration.java
@@ -2,9 +2,7 @@ package com.camunda.demo.springboot.conf;
 
 
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.cloud.Cloud;
-import org.springframework.cloud.CloudFactory;
-import org.springframework.cloud.service.common.AmqpServiceInfo;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,21 +10,10 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("cloud")
-public class RabbitMqCloudConfiguration {
+public class RabbitMqCloudConfiguration extends AbstractCloudConfig {
 
   @Bean
   public ConnectionFactory rabbitConnectionFactory() {
-    CloudFactory cloudFactory = new CloudFactory();
-    Cloud cloud = cloudFactory.getCloud();
-    AmqpServiceInfo serviceInfo = (AmqpServiceInfo) cloud.getServiceInfo("camunda-mq");
-    String serviceID = serviceInfo.getId();
-    ConnectionFactory connectionFactory = cloud.getServiceConnector(serviceID, ConnectionFactory.class, null);
-//    System.out.println(connectionFactory);
-    return connectionFactory;
-//    String uri = System.getenv("CLOUDAMQP_URL");
-//    if (uri == null) uri = "amqp://guest:guest@localhost";
-//
-//    ConnectionFactory factory = new ConnectionFactory();
-//    factory.setUri(uri);
+    return connectionFactory().rabbitConnectionFactory();
   }
 }
